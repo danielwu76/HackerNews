8d129153aa8c0cba0eb1d66d1e91b08f90805d2a
Daniel Wu <danielwu76@hotmail.com>
2025-01-22 00:02:03 -0800

    clean up the code

--- APP/src/app/app.component.ts
+++ APP/src/app/app.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  title = 'Hacker News Feed';
-  data: any[] = [];
+  title = 'Hacker News';
 
   constructor() { }
 
